34ada0c4b6d0c87461eb022544e46d9e430294bb
EdgarIbragimov <iem_04@mail.ru>
2025-04-21 22:22:12 +0300

    Book Filter Dto

---
+++ backend/src/main/java/org/example/backend/dto/BookFilterRequest.java
@@ -0,0 +1,23 @@
+package org.example.backend.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.backend.model.Country;
+import org.example.backend.model.Tag;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class BookFilterRequest {
+    private String title;
+    private List<Tag> tags;
+    private Double ratingFrom;
+    private Double ratingTo;
+    private Integer startYearFrom;
+    private Integer startYearTo;
+    private Integer quantityPagesFrom;
+    private Integer quantityPagesTo;
+    private List<String> authors;
+    private Country country;
+}
