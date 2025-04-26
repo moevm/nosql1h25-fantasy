98c342b48dc03a86c1dc0cd02af057243aa17921
Stepanov Dmitry <stepnik2408@gmail.com>
2025-04-26 19:25:25 +0300

    feat: upd footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,10 +1,140 @@
-import { Component } from '@angular/core';
-import { TuiButton, TuiGroup } from '@taiga-ui/core';
+import { Component, inject } from '@angular/core';
+import {
+  TuiAppearance,
+  TuiButton,
+  TuiDialog,
+  TuiGroup,
+} from '@taiga-ui/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TuiInputModule } from '@taiga-ui/legacy';
+import { TuiFileLike, TuiFiles } from '@taiga-ui/kit';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
-  imports: [TuiGroup, TuiButton],
+  imports: [
+    TuiGroup,
+    TuiButton,
+    TuiDialog,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiFiles,
+    FormsModule,
+    AsyncPipe,
+    NgIf,
+    TuiAppearance,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.less',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  protected open = false;
+
+  protected readonly control = new FormControl<TuiFileLike | null>(
+    null,
+    Validators.required
+  );
+
+  private readonly http = inject(HttpClient);
+
+  private readonly url = 'http://localhost:8080/catalog/import';
+
+  protected showDialog(): void {
+    this.open = true;
+  }
+
+  protected readonly failedFiles$ = new Subject<TuiFileLike | null>();
+  protected readonly loadingFiles$ =
+    new Subject<TuiFileLike | null>();
+  protected readonly loadedFiles$ = this.control.valueChanges.pipe(
+    switchMap(file => this.processFile(file))
+  );
+
+  protected removeFile(): void {
+    this.control.setValue(null);
+    this.failedFiles$.next(null);
+    this.loadingFiles$.next(null);
+  }
+
+  protected processFile(
+    file: TuiFileLike | null
+  ): Observable<TuiFileLike | null> {
+    if (this.control.invalid || !file) {
+      return of(null);
+    }
+
+    const isJson =
+      file.type === 'application/json' ||
+      (file.name && file.name.toLowerCase().endsWith('.json'));
+
+    if (!isJson) {
+      this.failedFiles$.next(file);
+      return of(null);
+    }
+
+    const reader = new FileReader();
+
+    const fileRead$ = new Observable<string>(observer => {
+      reader.onload = () => {
+        observer.next(reader.result as string);
+        observer.complete();
+      };
+      reader.onerror = error => {
+        observer.error(error);
+      };
+      reader.readAsText(file as File);
+    });
+
+    this.loadingFiles$.next(file);
+
+    return fileRead$.pipe(
+      switchMap(text => {
+        try {
+          const json = JSON.parse(text);
+          return this.http.post(this.url, json).pipe(
+            map(() => file),
+            catchError(() => {
+              this.failedFiles$.next(file);
+              return of(null);
+            })
+          );
+        } catch (e) {
+          console.error('Ошибка парсинга JSON', e);
+          this.failedFiles$.next(file);
+          return of(null);
+        }
+      }),
+      finalize(() => this.loadingFiles$.next(null))
+    );
+  }
+
+  protected exportData() {
+    this.http.get(this.url).subscribe({
+      next: (data: any) => {
+        const jsonString = JSON.stringify(data, null, 2);
+        const blob = new Blob([jsonString], {
+          type: 'application/json',
+        });
+        const url = window.URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'data.json';
+        link.click();
+
+        window.URL.revokeObjectURL(url);
+      },
+      error: (err: unknown) => {
+        console.error('Ошибка при отправке формы:', err);
+      },
+    });
+  }
+}
