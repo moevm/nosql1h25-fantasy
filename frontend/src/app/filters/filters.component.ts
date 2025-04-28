e37aec152f55616449ee1cda5cc79c05e91334f5
KaneFlak <kflak@yandex.ru>
2025-04-28 22:31:25 +0300

    Upd filter

--- frontend/src/app/filters/filters.component.ts
+++ frontend/src/app/filters/filters.component.ts
@@ -27,6 +27,7 @@ import { NgIf } from '@angular/common';
 import { Movie } from '../data-access/movie.service';
 import { Book } from '../data-access/book.service';
 import { Series } from '../data-access/series.service';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-filters',
@@ -63,21 +64,21 @@ export class FiltersComponent {
   private http = inject(HttpClient);
   private url = 'http://localhost:8080';
 
-  result: (Book | Movie | Series)[] | null = null;
+  result: (Book | Movie | Series)[] = [];
   selectedResult: Book | Movie | Series | null = null;
 
   protected readonly form = new FormGroup({
-    search: new FormControl(),
-    tagFilter: new FormControl(),
-    typeFilter: new FormControl(),
+    search: new FormControl(''),
+    tagFilter: new FormControl<string[]>([]),
+    typeFilter: new FormControl<string[]>([]),
     rating: new FormControl([0, 10]),
     yearRange: new FormControl([1800, 2025]),
     duration: new FormControl([0, 300]),
     quantity: new FormControl([0, 2000]),
     seasons: new FormControl([0, 20]),
-    author: new FormControl(),
-    director: new FormControl(),
-    actors: new FormControl(),
+    author: new FormControl(''),
+    director: new FormControl(''),
+    actors: new FormControl(''),
     country: new FormControl(),
   });
 
@@ -126,6 +127,43 @@ export class FiltersComponent {
     const quantity = this.form.get('quantity')?.getRawValue();
     const seasons = this.form.get('seasons')?.getRawValue();
 
+    const generalBody = {
+      title: title ? title : '',
+      tags: tags
+        ? tags.map((tag: string) =>
+            tag.toUpperCase().replace(/\s+/g, '_')
+          )
+        : [],
+      ratingFrom: rating[0],
+      ratingTo: rating[1],
+      startYearFrom: year[0],
+      startYearTo: year[1],
+      country: country,
+    };
+
+    const movieBody = {
+      ...generalBody,
+      durationFrom: duration[0],
+      durationTo: duration[1],
+      directors: [director],
+      actors: [actors],
+    };
+
+    const bookBody = {
+      ...generalBody,
+      quantityPagesFrom: quantity[0],
+      quantityPagesTo: quantity[1],
+      authors: [author],
+    };
+
+    const seriesBody = {
+      ...generalBody,
+      seasonsFrom: seasons[0],
+      seasonsTo: seasons[1],
+      directors: [director],
+      actors: [actors],
+    };
+
     console.log(
       `Searching for "${title}" with tags: ${tags}; type: ${types};
       rating: ${rating}; year: ${year}; country: ${country};
@@ -133,69 +171,43 @@ export class FiltersComponent {
       duration: ${duration}; quantity: ${quantity}; sesons: ${seasons}`
     );
 
-    this.http
-      .get<(Book | Movie | Series)[]>(this.url + '/alls', {
-        params: {
-          title: title ? title : '',
-          tags: tags
-            ? tags
-                .map((tag: string) =>
-                  tag.toUpperCase().replace(/\s+/g, '_')
-                )
-                .join(',')
-            : [],
-          ratingFrom: rating[0],
-          ratingTo: rating[1],
-          startYearFrom: year[0],
-          startYearTo: year[1],
-          country: country ? country : '',
-          page: 0,
-          size: 0,
-        },
-      })
-      .subscribe(res => {
-        this.result = res.filter(
-          el =>
-            (types == null ||
-              types.length == 0 ||
-              types.some(
-                (t: string) =>
-                  t.toLowerCase() === el.type.toLowerCase()
-              )) &&
-            (author == null ||
-              author == '' ||
-              el.persons.some(
-                person =>
-                  person.role === 'AUTHOR' &&
-                  person.name.match(author)
-              )) &&
-            (director == null ||
-              director == '' ||
-              el.persons.some(
-                person =>
-                  person.role === 'DIRECTOR' &&
-                  person.name.match(director)
-              )) &&
-            (actors == null ||
-              actors == '' ||
-              el.persons.some(
-                person =>
-                  person.role === 'ACTOR' && person.name.match(actors)
-              )) &&
-            (el.type !== 'BOOK' ||
-              (quantity[0] <= (el as Book).quantityPages &&
-                (el as Book).quantityPages <= quantity[1])) &&
-            (el.type !== 'FILM' ||
-              (duration[0] <= (el as Movie).duration &&
-                (el as Movie).duration <= duration[1])) &&
-            (el.type !== 'SERIES' ||
-              (seasons[0] <= (el as Series).seasons &&
-                (el as Series).seasons <= seasons[1]))
-        );
+    console.log(movieBody, bookBody, seriesBody);
 
-        console.log(this.result);
-        this.cdr.detectChanges();
-      });
+    const tasks$ = [];
+
+    if (!types.length || types.includes('Book')) {
+      tasks$.push(
+        this.http.post<Book[]>(this.url + '/books/search', bookBody, {
+          params: { page: 0, size: 0 },
+        })
+      );
+    }
+    if (!types.length || types.includes('Film')) {
+      tasks$.push(
+        this.http.post<Movie[]>(
+          this.url + '/movies/search',
+          movieBody,
+          { params: { page: 0, size: 0 } }
+        )
+      );
+    }
+    if (!types.length || types.includes('Series')) {
+      tasks$.push(
+        this.http.post<Series[]>(
+          this.url + '/series/search',
+          seriesBody,
+          { params: { page: 0, size: 0 } }
+        )
+      );
+    }
+
+    this.result = [];
+    zip(...tasks$).subscribe(results => {
+      results.forEach(
+        res => (this.result = [...this.result, ...res])
+      );
+      this.cdr.detectChanges();
+    });
   }
 
   protected selectResult(res: Book | Movie | Series) {
