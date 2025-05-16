22b7fa0ca98519980b9ce1470009070800ba311c
KaneFlak <kflak@yandex.ru>
2025-05-16 18:27:07 +0300

    Add styles for detail component

--- frontend/src/app/shell/shell.component.ts
+++ frontend/src/app/shell/shell.component.ts
@@ -2,12 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppHeaderComponent } from '../app-header/app-header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { tuiDialog } from '@taiga-ui/core';
+import { TuiButton, tuiDialog } from '@taiga-ui/core';
 import { AddDataModalComponent } from '../add-data-modal/add-data-modal.component';
 
 @Component({
   selector: 'app-shell',
-  imports: [AppHeaderComponent, FooterComponent, RouterOutlet],
+  imports: [AppHeaderComponent, FooterComponent, RouterOutlet, TuiButton],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
